8550bd66a6173e372312d9e81ee63f20692c5d30
jfk94 <sekwon.jang@obigo.com>
2018-01-30 17:58:35 +0900

    added startup script for copying AF

--- Setup.py
+++ Setup.py
@@ -4,11 +4,15 @@ import os
 import subprocess
 import shutil
 from pylib.print_color import *
+from pylib.copytree import *
 
 UICCID_PATH = '/etc/sa/'
 UICCID_FILE = 'uiccid.txt'
 NAVI_RUN_SCRIPT = 'run_navi.sh'
 NAVI_PATH = '/home/telecons/work/SYMC_C300'
+AF_TARGET_PATH = '/home/vagrant/Release/Obigo/OAF'
+AF_SOURCE_PATH = '/home/vagrant/work/Release/Obigo/OAF'
+AF_W20 = 'w20'
 
 def checkPath(path, exist):
 	if exist == True:
@@ -42,3 +46,19 @@ printheader("##########################################################")
 checkPath(os.path.join(CWD, NAVI_RUN_SCRIPT), True)
 checkPath(NAVI_PATH, True)
 subprocess.call(['sudo', 'cp', os.path.join(CWD, NAVI_RUN_SCRIPT), os.path.join(NAVI_PATH, NAVI_RUN_SCRIPT)]);
+
+printheader("##########################################################")
+printheader("### Copying AF from %s to %s" % (AF_SOURCE_PATH, AF_TARGET_PATH))
+printheader("##########################################################")
+checkPath(AF_SOURCE_PATH, True)
+if os.path.exists(AF_TARGET_PATH) == False:
+	copytree(AF_SOURCE_PATH, AF_TARGET_PATH)
+else:
+	SOURCE_TIME = os.path.getmtime(os.path.join(AF_SOURCE_PATH, AF_W20))
+	TARGET_TIME = os.path.getmtime(os.path.join(AF_TARGET_PATH, AF_W20))
+	printheader("Source Time : %s , Target Time : %s" % (SOURCE_TIME, TARGET_TIME))
+	if SOURCE_TIME != TARGET_TIME:
+		printheader("Remove Target : " + AF_TARGET_PATH)
+		shutil.rmtree(AF_TARGET_PATH)
+		printheader("### Copying AF from %s to %s" % (AF_SOURCE_PATH, AF_TARGET_PATH))
+		copytree(AF_SOURCE_PATH, AF_TARGET_PATH)
